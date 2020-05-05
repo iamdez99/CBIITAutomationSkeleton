c92930aed5b34858553b0da47a43c45b5162558c
iamdez99 <diegojuarezbusiness@gmail.com>
2020-05-05 16:06:23 -0400

    Pushing CBITT Automation Skeleton

---
+++ src/test/java/com/CBIIT/testbase/BaseClass.java
@@ -0,0 +1,49 @@
+package com.CBIIT.testbase;
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.COVIDcode.utils.ConfigsReader;
+import com.COVIDcode.utils.Constants;
+
+public class BaseClass {
+
+	public static WebDriver driver;
+
+
+	public static void setUp() {
+		ConfigsReader.readProperties(Constants.CREDENTIALS_FILEPATH);
+		
+		switch(ConfigsReader.getProperty("browser").toLowerCase()) {
+		case "chrome" :
+			System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH);
+			driver = new ChromeDriver();
+			break;
+		case "firefox" : 
+			System.setProperty("webdriver.gecko.driver", Constants.GECKO_DRIVER_PATH);
+			driver = new FirefoxDriver();
+			break;
+		default : 
+			System.err.println("Browser is not supported");
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Constants.PAGE_LOAD_TIME, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_LOAD_TIME, TimeUnit.SECONDS);
+		
+		driver.get(ConfigsReader.getProperty("COVIDcodeTestURL"));
+		
+	}
+	
+
+	public static void closeBrowser() {
+		if(driver!= null) {
+			driver.quit();
+		}
+	}
+	
+}
